7556efea233795c9071256a0ffa94a7daf1c4aaa
MaoSu <45323874+MaoSuSu@users.noreply.github.com>
2022-04-22 15:36:18 +0800

    更新

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
-#
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 修改默认IP
+sed -i 's/10.10.10.1/192.168.1.1/g' package/base-files/files/bin/config_generate
+
+# 修改默认登录密码
+sed -i 's/root:$1$WplwC1t5$HBAtVXABp7XbvVjG4193B.:18753:0:99999:7:::/root::0:0:99999:7:::/g' package/base-files/files/etc/shadow
+
+# 修改主机名
+sed -i 's/OpenWrt/MaoSuOS/g' package/base-files/files/bin/config_generate
+
+# 修改WIFI名
+sed -i 's/OpenWrt/MaoSu/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
