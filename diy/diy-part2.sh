75435bd2f3b70fb889a03dbcd6343f44534f1393
MaoSu <45323874+MaoSuSu@users.noreply.github.com>
2022-10-05 01:35:50 +0800

    1

--- diy/diy-part2.sh
+++ diy/diy-part2.sh
@@ -9,9 +9,3 @@ sed -i 's/root:$1$WplwC1t5$HBAtVXABp7XbvVjG4193B.:18753:0:99999:7:::/root::0:0:9
 
 # 修改主机名
 sed -i 's/OpenWrt/MaoSuOS/g' package/base-files/files/bin/config_generate
-
-# 修改WIFI名
-sed -i 's/OpenWrt/MaoSu/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-
-# 修改WIFI区域代码
-sed -i 's/US/CN/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
