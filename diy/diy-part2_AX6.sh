0741f8fdd0c2c3362977baeadefc3acc0691c881
MaoSu <45323874+MaoSuSu@users.noreply.github.com>
2022-10-22 21:47:55 +0800

    1

--- diy/diy-part2_AX6.sh
+++ diy/diy-part2_AX6.sh
@@ -1,9 +1,6 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 
-# 修改默认IP
-sed -i 's/10.10.10.1/192.168.1.1/g' package/base-files/files/bin/config_generate
-
 # 修改默认登录密码
 sed -i 's/root:$1$WplwC1t5$HBAtVXABp7XbvVjG4193B.:18753:0:99999:7:::/root::0:0:99999:7:::/g' package/base-files/files/etc/shadow
 
@@ -19,3 +16,6 @@ sed -i 's/US/CN/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 # 注释检查软件签名并添加自定义软件源
 sed -i 's/check_signature 1/check_signature 0/g' package/system/opkg/files/opkg-smime.conf
 echo 'src/gz openwrt_kiddin9 https://op.supes.top/packages/aarch64_cortex-a53' >>package/system/opkg/files/customfeeds.conf
+
+#修正连接数
+sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
